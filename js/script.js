5963c528857f7dc245f958e98f03e00ad3bda48f
Alexandr Pavlychenko <apavlychenko23@gmail.com>
2023-05-29 16:15:44 +0300

    fix: corrects pseudotitle content

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-const swiperEl = document.querySelector('.swiper');
-
-const swiper = new Swiper(swiperEl, {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.slider__control--next',
-    prevEl: '.slider__control--prev',
-  },
-});
+const swiperEl=document.querySelector(".swiper"),swiper=new Swiper(swiperEl,{loop:!0,slidesPerView:1,navigation:{nextEl:".slider__control--next",prevEl:".slider__control--prev"}});
